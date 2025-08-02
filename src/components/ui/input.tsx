3157a03a85648a14811e18fe40caaea43cbc4eae
Amin F <afiroozi007@gmail.com>
2025-08-02 08:27:27 +0000

    در فرم ثبت نام این عملکرد را نمیبینم

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,9 +1,25 @@
-import * as React from "react"
 
+"use client"
+
+import * as React from "react"
 import { cn } from "@/lib/utils"
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, onChange, ...props }, ref) => {
+    const [direction, setDirection] = React.useState('rtl');
+
+    const handleDirectionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const persianRegex = /[\u0600-\u06FF]/;
+      if (persianRegex.test(e.target.value)) {
+        setDirection('rtl');
+      } else {
+        setDirection('ltr');
+      }
+      if (onChange) {
+        onChange(e);
+      }
+    };
+
     return (
       <input
         type={type}
@@ -12,6 +28,8 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
           className
         )}
         ref={ref}
+        onChange={handleDirectionChange}
+        style={{ direction }}
         {...props}
       />
     )
